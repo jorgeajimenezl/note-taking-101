5932e16ddfc479047205d5f3e79a1c0443af47e5
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2024-11-18 10:27:05 +0000

    Add Tag model, factory, and migration for tags table and pivot table with tasks

---
+++ database/migrations/2024_11_18_101757_create_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('tag_task', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tag_id')->constrained();
+            $table->foreignId('task_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('tag_task');
+    }
+};
