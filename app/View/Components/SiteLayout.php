d370e1fa250182ffe775fee98e9914b98c5cc078
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2024-11-21 08:34:04 +0000

    Add layout components and welcome controller for improved view structure

---
+++ app/View/Components/SiteLayout.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class SiteLayout extends Component
+{
+    public $title;
+
+    public function __construct(?string $title = null)
+    {
+        $this->title = ucfirst($title);
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('layouts.site.layout');
+    }
+}
