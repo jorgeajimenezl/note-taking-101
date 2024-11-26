ffd47ab280402f3a0483d2cd99ed534b690931d0
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2024-11-26 13:31:53 +0000

    Install breeze to manage all the authentication logic

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -2,16 +2,15 @@
 
 namespace App\View\Components;
 
-use Closure;
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Illuminate\View\View;
 
 class AppLayout extends Component
 {
     /**
-     * Get the view / contents that represent the component.
+     * Get the view / contents that represents the component.
      */
-    public function render(): View|Closure|string
+    public function render(): View
     {
         return view('layouts.app');
     }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Tasks
 Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('task.index');
 Route::get('/task/{id}', [App\Http\Controllers\TaskController::class, 'show'])->name('task.show');
 Route::put('/task/{id}', [App\Http\Controllers\TaskController::class, 'update'])->name('task.update');
 Route::delete('/task/{id}', [App\Http\Controllers\TaskController::class, 'destroy'])->name('task.destroy');
 Route::post('/task/{task}/toggle-complete', [App\Http\Controllers\TaskController::class, 'toggleComplete']);
+
+require __DIR__.'/auth.php';
