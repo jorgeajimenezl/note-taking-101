1cd5fdb48628665668e03eb551d22b7d5e550ccc
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2024-11-27 08:19:20 +0000

    Enhance tags index view to display user-specific tags and add create/delete functionality

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
+
 class TagController extends Controller
 {
     public function index()
     {
-        return view('tags.index');
+        $tags = Tag::where('user_id', auth()->id())->get();
+
+        return view('tags.index', compact('tags'));
     }
 }
--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -1,7 +1,23 @@
 <x-app-layout title="Tags">
     <div class="container mx-auto flex justify-center bg-white rounded-lg shadow-lg my-5 max-w-screen-md p-5">
-        <ul class="w-3/4">
-            
+        <div class="flex justify-end mb-4">
+            <a href="{{ route('tags.create') }}" class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">Create</a>
+        </div>
+
+        <ul class="w-3/4 flex flex-wrap">
+            @foreach ($tags as $tag)
+            <li class="border-b border-gray-300 py-2 bg-gray-200 mt-2.5 p-2.5 rounded flex items-center mr-2 mb-2">
+                <i class="fa-solid fa-tag mr-2"></i>
+                <span class="text-base mr-2">{{ $tag->name }}</span>
+                <form action="{{ route('tags.destroy', $tag->id) }}" method="POST" class="inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="text-red-500 hover:text-red-700">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                </form>
+            </li>
+            @endforeach
         </ul>
     </div>
 </x-app-layout>
