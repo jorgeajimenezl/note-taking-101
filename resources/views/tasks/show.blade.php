844c9db1929f85804f5007004726bcdead187ebe
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2024-11-28 17:38:54 +0000

    Adjust spacing for save button in task show view for improved layout

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -14,7 +14,7 @@
                     <div class="title-section flex items-center justify-between">
                         <x-text-input id="task-title" data-mark="input-field" class="text-3xl font-bold tracking-tight text-gray-900 bg-transparent border-none focus:outline-none transition duration-300 ease-in-out flex-grow" value="{{$task->title}}" name="title" readonly />
                         @if($role !== 'viewer')
-                            <x-primary-button id="save-button" class="ml-4">Save Changes</x-primary-button>
+                            <x-primary-button id="save-button" class="ml-2">Save Changes</x-primary-button>
                             <x-primary-button id="share-button" class="ml-2" type="button" onclick="toggleContributorDialog(true)">Share</x-primary-button>
                             <x-danger-button id="delete-button" class="ml-2">
                                 <i class="fas fa-trash-alt"></i> 
