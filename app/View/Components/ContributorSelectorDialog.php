c2e428e7ac0f351b07c7bce0d0b21d43de301e95
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2024-11-28 17:04:04 +0000

    Refactor ContributorSelectorDialog to retrieve contributors based on task ID, simplifying the component's constructor and enhancing data handling in the task show view.

--- app/View/Components/ContributorSelectorDialog.php
+++ app/View/Components/ContributorSelectorDialog.php
@@ -2,16 +2,20 @@
 
 namespace App\View\Components;
 
+use App\Models\Task;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class ContributorSelectorDialog extends Component
 {
+    public array $contributors;
+
     public function __construct(
-        public array $contributors,
         public int $taskId,
-    ) {}
+    ) {
+        $this->contributors = Task::find($this->taskId)->contributors->all();
+    }
 
     public function render(): View|Closure|string
     {
