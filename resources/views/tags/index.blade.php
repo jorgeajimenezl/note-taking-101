708aff0b0cbc736bb882e9ba948f013f89a6836e
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2024-11-28 20:57:52 +0000

    Update link in tags index view to point to tag creation route

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout title="Tags">
     <div class="flex justify-end my-5 max-w-screen-md mx-auto">
-        <a href="{{ route('tasks.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Create</a>
+        <a href="{{ route('tags.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Create</a>
     </div>
     <div class="container mx-auto bg-white rounded-lg shadow-lg my-5 max-w-screen-md p-5">
         <ul class="w-3/4 flex flex-wrap">
