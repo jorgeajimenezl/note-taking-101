d211e5049d828ac3f276956ee04311329ae2cb68
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-25 17:46:00 +0000

    Add Artisan commands for user management: create, delete, and verify

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,32 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('create:user {email}', function ($email) {
+    $password = $this->secret('Enter the password');
+    $name = $this->ask('Enter the name');
+    $this->info("Creating user with email: $email, name: $name");
+
+    User::create([
+        'name' => $name,
+        'email' => $email,
+        'password' => Hash::make($password),
+    ]);
+})->purpose('Create a new user');
+
+Artisan::command('delete:user {email}', function ($email) {
+    $this->info("Deleting user with email: $email");
+    User::where('email', $email)->delete();
+})->purpose('Delete a user');
+
+Artisan::command('verify:user {email}', function ($email) {
+    $this->info("Verifying user with email: $email");
+    User::where('email', $email)->update(['email_verified_at' => now()]);
+})->purpose('Verify a user');
