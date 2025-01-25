70c374fe1f3963d5f5c28bd678adab9f23b4e0d5
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-25 17:46:10 +0000

    Refactor DatabaseSeeder to simplify data seeding process by removing unused factories and associations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\Tag;
-use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         User::create([
@@ -22,24 +17,5 @@ class DatabaseSeeder extends Seeder
             // 'is_admin' => true,
         ]);
 
-        User::factory(10)->create();
-        Task::factory(10)->create();
-        Tag::factory(30)->create();
-
-        // Give each task between 1 and 4 tags
-        foreach (Task::all() as $task) {
-            $tags = Tag::where('user_id', $task->user_id)
-                ->inRandomOrder()
-                ->take(random_int(1, 4))
-                ->get();
-
-            $task->tags()->attach($tags);
-        }
-
-        // Create contributors
-        foreach (Task::all() as $task) {
-            $contributors = User::whereNot('id', $task->user_id)->inRandomOrder()->take(random_int(1, 10))->get();
-            $task->contributors()->attach($contributors, ['role' => fake()->randomElement(['editor', 'viewer'])]);
-        }
     }
 }
