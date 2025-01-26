60c2b3a1c12e253ec828569bb745ccc0234b2e08
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-26 12:33:31 +0000

    Remove wire:submit.prevent from task form to allow default submission behavior

--- resources/views/livewire/task-details.blade.php
+++ resources/views/livewire/task-details.blade.php
@@ -1,4 +1,4 @@
-<form id="task-form" wire:submit.prevent="saveTask">
+<form id="task-form">
     <div class="container mx-auto bg-white rounded-lg shadow-lg my-5 max-w-screen-md p-5">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <div class="title-section flex items-center justify-between">
