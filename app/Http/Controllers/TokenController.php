dc47beb3ef828d2d4919e606bcc20eca1d593059
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-26 03:18:38 +0000

    Add TokenController and views for managing API tokens

---
+++ app/Http/Controllers/TokenController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TokenController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validated = $request->validateWithBag('storeToken', [
+            'name' => ['required'],
+        ]);
+
+        $token = $request->user()->createToken($validated['name']);
+
+        return back()->with('token', $token->plainTextToken);
+    }
+
+    public function destroy(Request $request, $token)
+    {
+        $request->user()->tokens()->where('id', $token)->delete();
+
+        return back();
+    }
+
+    public function index(Request $request)
+    {
+        return view('tokens.index', [
+            'tokens' => $request->user()->tokens,
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\ContributorController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TokenController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -24,6 +25,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('/tasks', TaskController::class)->except(['edit']);
     Route::resource('/tags', TagController::class)->except(['edit', 'update', 'show']);
     Route::post('/tasks/{task}/toggle-complete', [TaskController::class, 'toggleComplete'])->name('tasks.toggle-complete');
+
+    // Token-based API authentication
+    Route::post('/tokens', [TokenController::class, 'store'])->name('tokens.store');
+    Route::delete('/tokens/{token}', [TokenController::class, 'destroy'])->name('tokens.destroy');
+    Route::get('/tokens', [TokenController::class, 'index'])->name('tokens.index');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
