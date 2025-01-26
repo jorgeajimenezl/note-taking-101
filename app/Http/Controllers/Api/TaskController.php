66b6df3503c9e65039e2732d58bff50f3d784cbf
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-26 03:54:47 +0000

    Enhance TaskController to include shared tasks for the authenticated user and update authorization check logic

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -16,8 +16,12 @@ class TaskController extends Controller
     public function index()
     {
         $tasks = Task::where('author_id', auth()->id())->paginate(10);
+        $sharedTasks = Task::select('tasks.*')
+            ->join('contributors', 'tasks.id', '=', 'contributors.task_id')
+            ->where('contributors.user_id', auth()->id())
+            ->paginate(10);
 
-        return new TaskCollection($tasks);
+        return new TaskCollection($tasks->merge($sharedTasks));
     }
 
     /**
@@ -35,7 +39,7 @@ class TaskController extends Controller
     {
         $task = Task::with('author', 'contributors', 'tags')->findOrFail($id);
 
-        if ($task->author_id !== auth()->id()) {
+        if ($task->getUserRole(auth()->user()) === null) {
             return response()->json(['message' => 'Unauthorized'], 403);
         }
 
--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -14,6 +14,7 @@ class TaskResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        ray($this->contributors);
         return [
             'id' => $this->id,
             'title' => $this->title,
