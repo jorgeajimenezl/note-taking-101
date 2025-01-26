c2208139c72ff46a6f0c114f101041f6ac58b44c
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-26 17:08:33 +0000

    Add Passkeys management section to profile edit view and enhance UI for passkey creation

--- resources/views/vendor/passkeys/livewire/passkeys.blade.php
+++ resources/views/vendor/passkeys/livewire/passkeys.blade.php
@@ -1,41 +1,47 @@
-<div>
-    <h1>Passkeys</h1>
-    <div class="mt-2">
-        <form id="passkeyForm" wire:submit="validatePasskeyProperties" class="flex items-center space-x-2">
-            <div>
-                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-                <input autocomplete="off" type="text" wire:model="name" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                @error('name')
-                <span class="text-red-500 text-sm">{{ $message }}</span>
-                @enderror
-            </div>
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900">
+            {{ __('Manage Passkeys') }}
+        </h2>
 
-            <button type="submit" class="mt-6 inline-flex justify-center py-2 px-4 font-medium">
-                Create
-            </button>
-        </form>
-    </div>
+        <p class="mt-1 text-sm text-gray-600">
+            {{ __('Passkeys allow for a more secure, seamless authentication experience on supported devices.') }}
+        </p>
+    </header>
 
+    <form name="createPasskey" wire:submit="validatePasskeyProperties"  class="mt-6 space-y-6">
+        @csrf
+
+        <div>
+            <x-input-label for="create_passkey_passkey_name" :value="__('Passkey Name')"/>
+            <x-text-input id="create_passkey_passkey_name" name="name" class="mt-1 block w-full" wire:model="name"/>
+            <x-input-error :messages="$errors->passkeyForm->get('name')" class="mt-2"/>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <x-primary-button>{{ __('Create Passkey') }}</x-primary-button>
+        </div>
+    </form>
+
+    @if(! $passkeys->isEmpty())
     <div class="mt-6">
-        <ul class="space-y-4">
+        <h3 class="font-medium text-gray-900">{{ __('Your Passkeys') }}</h3>
+        <ul class="mt-2">
             @foreach($passkeys as $passkey)
-                <li class="flex justify-between items-center p-4 bg-gray-100 rounded-lg shadow-sm">
-                    <div class="text-gray-700">
-                        {{ $passkey->name }}
+                <li class="px-2 py-2 flex justify-between items-center">
+                    <div class="flex flex-col">
+                        <span class="font-semibold">{{ $passkey->name }}</span>
+                        <span class="font-thin text-sm text-gray-600">Added {{ $passkey->created_at->diffForHumans() }}</span>
                     </div>
-                    <div class="ml-2">
-                        Last used: {{ $passkey->last_used_at?->diffForHumans() ?? 'Not used yet' }}
-                    </div>
-                    <div>
-                        <button wire:click="deletePasskey({{ $passkey->id }})" class="inline-flex justify-center py-2 px-4 text-sm font-medium text-white bg-red-600">
-                            Delete
-                        </button>
-                    </div>
+
+                    <x-danger-button class="" wire:click="deletePasskey({{ $passkey->id }})">
+                        Remove
+                    </x-danger-button>
                 </li>
             @endforeach
         </ul>
     </div>
+    @endif
 
     @include('passkeys::livewire.partials.createScript')
-</div>
-
+</section>
