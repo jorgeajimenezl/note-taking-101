eb1eeb52916d7d947c9d0eaf870f5ec8b098b420
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-26 01:57:38 +0000

    Add TaskController and resource classes for task management API

---
+++ app/Http/Resources/TaskCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class TaskCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return $this->collection->map(function ($task) {
+            return [
+                'id' => $task->id,
+                'title' => $task->title,
+            ];
+        })->all();
+    }
+}
