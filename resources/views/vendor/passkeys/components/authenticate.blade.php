c3791615c2b2bdcc706a81bd3024d484022216cf
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-26 16:47:50 +0000

    Enhance Passkey authentication button with error handling and visibility control

--- resources/views/vendor/passkeys/components/authenticate.blade.php
+++ resources/views/vendor/passkeys/components/authenticate.blade.php
@@ -11,13 +11,27 @@
         </div>
     @endif
 
-   <!--
-    if (! browserSupportsWebAuthn()) {
-    return;
-    }
-   -->
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            if (browserSupportsWebAuthn()) {
+                document.getElementById('passkey-button').style.display = 'block';
+            }
+        });
 
-    <button onclick="event.preventDefault(); authenticateWithPasskey()" {{ $attributes }}>
+        function authUsingPasskey() {
+            console.log('Authenticating using Passkey...');
+            authenticateWithPasskey()
+                .then(response => {
+                    console.log("Authentication successful");
+                })
+                .catch(error => {
+                    console.error(error);
+                    document.getElementById('passkey-error-message').innerText = 'Authentication using passkey failed. Please try again.';
+                });
+        }
+    </script>
+
+    <button id="passkey-button" style="display: none;" onclick="event.preventDefault(); authUsingPasskey()" {{ $attributes }}>
         @if ($slot->isEmpty())
             <div class="underline cursor-pointer">
                 Authenticate using Passkey
@@ -26,4 +40,5 @@
             {{ $slot }}
         @endif
     </button>
+    <div id="passkey-error-message" class="text-red-500 mt-2"></div>
 </div>
