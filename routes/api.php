ff21084e7ee4767d2227162f66a7a4c0b20f3ceb
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-26 03:26:10 +0000

    Rename user route to /me for clarity and consistency

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/me', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
