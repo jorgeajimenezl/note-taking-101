e499b6a958e0b8e9d6e979cf97f2519a92de44a0
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-28 10:55:06 +0000

    Make SharedTask notification queueable to avoid latency

--- app/Notifications/SharedTask.php
+++ app/Notifications/SharedTask.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Task;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SharedTask extends Notification
+class SharedTask extends Notification implements ShouldQueue
 {
     use Queueable;
 
