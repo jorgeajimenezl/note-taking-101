87e2978d2ac0ed9b40a86e1bc4e42ab37748ffa0
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-28 14:45:02 +0000

    Disable sundays backups in order to avoid recursive backups

--- routes/console.php
+++ routes/console.php
@@ -33,4 +33,4 @@ Artisan::command('verify:user {email}', function ($email) {
 
 Schedule::command('backup:clean')->dailyAt('03:10')->timezone('Europe/Madrid');
 Schedule::command('backup:run --only-db')->dailyAt('03:15')->timezone('Europe/Madrid');
-Schedule::command('backup:run')->sundays()->at('03:20')->timezone('Europe/Madrid');
+// Schedule::command('backup:run')->sundays()->at('03:20')->timezone('Europe/Madrid');
