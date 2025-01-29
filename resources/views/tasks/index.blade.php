85ee027d3ae3323aefcb9ad838f53b8d2824e8f8
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-29 14:07:52 +0000

    Add Google icon to import button in tasks index view

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,6 +1,9 @@
 <x-app-layout title="Tasks">
     <div class="flex justify-end my-5 max-w-screen-md mx-auto">
-        <a href="{{ route('import.google-tasks') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 mr-2">Import</a>
+        <a href="{{ route('import.google-tasks') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 mr-2">
+            <i class="fa-brands fa-google mr-2"></i>    
+            Import
+        </a>
         <a href="{{ route('tasks.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Create</a>
     </div>
 
