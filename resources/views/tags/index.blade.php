29be175bae9e71da03fe9c4bd2b5d9b11f968275
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-01-29 10:14:36 +0000

    Enhance tags index layout with improved heading and styling for tag items

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -3,16 +3,18 @@
         <a href="{{ route('tags.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Create</a>
     </div>
     <div class="container mx-auto bg-white rounded-lg shadow-lg my-5 max-w-screen-md p-5">
-        <ul class="w-3/4 flex flex-wrap">
+        <h2 class="text-2xl font-semibold text-gray-800 mb-4">Tags</h2>
+        <ul class="flex flex-wrap gap-4">
             @foreach ($tags as $tag)
-                <li class="border-b border-gray-300 py-2 bg-gray-200 mt-2.5 p-2.5 rounded flex items-center mr-2 mb-2">
-                    <i class="fa-solid fa-tag mr-2"></i>
-                    <span class="text-base mr-2">{{ $tag->name }}</span>
+                <li class="border border-gray-300 py-1 px-4 bg-gray-200 rounded-full flex items-center justify-between">
+                    <div class="flex items-center">
+                        <span class="text-base font-medium text-gray-800">{{ $tag->name }}</span>
+                    </div>
                     <form action="{{ route('tags.destroy', $tag->id) }}" method="POST" class="inline">
                         @csrf
                         @method('DELETE')
                         <button type="submit" class="ml-2 text-red-500 hover:text-red-700">
-                            &times;
+                            <i class="fa-solid fa-times"></i>
                         </button>
                     </form>
                 </li>
